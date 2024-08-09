b07829e687ca0c1cb22ecf2de551ad83056be4f7
andrewpressman <ap@b-p.com>
2024-08-09 12:38:49 -0400

    Initial commit

---
+++ DesktopController.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+#Windows to be shown/removed
+#var Options = #prelead options
+var Numpad = preload("res://Tasks/Numpad.tscn")
+#var FileManager = preload("res://Tasks/Numpad.tscn")
+#var Messenger = preload("res://Tasks/Numpad.tscn")
+#var Memo = preload("res://Tasks/Numpad.tscn")
+#var Buttons = preload("res://Tasks/Numpad.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event.is_key_pressesd(KEY_ESCAPE):
+		#Show Options Window
+		pass
+
+func DisplayNumpad(view : bool):
+	if view:
+		add_child(Numpad.instantiate())
+	else:
+		pass
+		
+#TESTING
+#new line
