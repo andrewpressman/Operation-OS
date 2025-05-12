3838a37ac8dc5353609d91d3a3714cf709c00871
andrewpressman <ap@b-p.com>
2025-05-12 13:57:40 -0400

    added Tutorial screen framework, labeled task windows

--- Menu/NewGameIntro.gd
+++ Menu/NewGameIntro.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
+func _ready() -> void:
+	$Intro.visible = true
+	$Tutorial.visible = false
+
 func Start():
 	get_tree().change_scene_to_file("res://Home/HomeDesktop.tscn")
+
+func Next():
+	$Intro.visible = false
+	$Tutorial.visible = true
